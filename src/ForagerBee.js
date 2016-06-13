3274198cc6e06450d827a8ba0dfccebe1f22d3bd
jchiou <jessica.chiou@gmail.com>
2016-06-13 15:03:13 -0700

    Add forage method to ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -5,4 +5,8 @@ var ForagerBee = function() {
   this.treasureChest = [];
 };
 
-ForagerBee.prototype = Object.create(Bee.prototype)
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+}
